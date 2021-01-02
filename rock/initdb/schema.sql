1c3a1c3730a51216eed72d9436b1c3d651a85c0d
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2021-01-02 10:12:34 -0500

    Add draft api

---
+++ rock/initdb/schema.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE dataset (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL UNIQUE,
+    path TEXT NOT NULL
+);
+
+CREATE TABLE pipeline (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL
+);
+
+CREATE TABLE transform (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    pipeline_id UUID REFERENCES pipeline(id),
+    name TEXT NOT NULL
+);
