39225ca2521c5412f1526ac9fced384a23710f11
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2021-01-03 09:44:27 -0500

    Use grid on pipeline list page

--- iron/pages/pipeline/index.js
+++ iron/pages/pipeline/index.js
@@ -2,20 +2,31 @@ import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { Typography } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
+import Icon from "@material-ui/core/Icon";
+import Grid from "@material-ui/core/Grid";
 
 export default function Pipelines({ pipelines }) {
   return (
-    <>
-      <Typography variant="h6">Pipelines</Typography>
-      <List>
-        {pipelines.map((p) => (
-          <ListItem key={p.id}>
-            <ListItemText primary={p.name} />
-          </ListItem>
-        ))}
-      </List>
-    </>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="h6">Pipelines</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" endIcon={<Icon>add</Icon>}>
+          Add
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <List>
+          {pipelines.map((p) => (
+            <ListItem key={p.id}>
+              <ListItemText primary={p.name} />
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    </Grid>
   );
 }
 
