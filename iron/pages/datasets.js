cb30606957314af29a356558f979542c5a696980
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2021-01-03 09:18:29 -0500

    Load data from backend

--- iron/pages/datasets.js
+++ iron/pages/datasets.js
@@ -1,5 +1,30 @@
 import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
-export default function About() {
-  return <>Datasets</>;
+export default function About({ datasets }) {
+  return (
+    <>
+      Datasets
+      <List>
+        {datasets.map((d) => (
+          <ListItem key={d.id}>
+            <ListItemText primary={d.name} />
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch("http://rest:3000/dataset");
+  const datasets = await res.json();
+
+  return {
+    props: {
+      datasets,
+    },
+  };
 }
--- iron/pages/pipelines.js
+++ iron/pages/pipelines.js
@@ -1,5 +1,27 @@
 import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
-export default function About() {
-  return <>Pipelines</>;
+export default function Pipelines({ pipelines }) {
+  return <>Pipelines
+  <List>
+    {pipelines.map((p) => (
+      <ListItem key={p.id}>
+        <ListItemText primary={p.name} />
+      </ListItem>
+    ))}
+  </List></>;
+}
+
+
+export async function getServerSideProps(context) {
+  const res = await fetch("http://rest:3000/pipeline");
+  const pipelines = await res.json();
+
+  return {
+    props: {
+      pipelines,
+    },
+  };
 }
