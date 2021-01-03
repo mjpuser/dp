ef80b2c36c90d454a5eb12871dc6de4b5c367234
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2021-01-03 11:08:24 -0500

    New pipeline draft

--- iron/pages/pipeline/new.js
+++ iron/pages/pipeline/new.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import LensIcon from "@material-ui/icons/Lens";
+import React from "react";
+import Tree from "react-d3-tree";
 
 export default function NewPipeline() {
-    return <>New Pipeline</>;
+  const graph = {
+    name: "root",
+    children: [
+        {name: 'transform 1'},
+        {name: 'transform 2'}
+    ],
+  };
+  return (
+    <div id="treeWrapper" style={{ width: "100%", height: "100vh" }}>
+      New Pipeline
+      <Tree zoomable={false} translate={{x: 200, y: 300 }} data={graph} />
+    </div>
+  );
 }
