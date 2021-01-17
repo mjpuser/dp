9c582659a35a3108a8ba57d1dc28747f5df2b0bc
Matthew Prestifilippo <matthew.prestifilippo@syapse.com>
2021-01-16 21:17:18 -0500

    Remove react tree dep

--- iron/pages/pipeline/new.js
+++ iron/pages/pipeline/new.js
@@ -1,19 +1,8 @@
-import LensIcon from "@material-ui/icons/Lens";
-import React from "react";
-import Tree from "react-d3-tree";
 
 export default function NewPipeline() {
-  const graph = {
-    name: "root",
-    children: [
-        {name: 'transform 1'},
-        {name: 'transform 2'}
-    ],
-  };
   return (
-    <div id="treeWrapper" style={{ width: "100%", height: "100vh" }}>
+    <div>
       New Pipeline
-      <Tree zoomable={false} translate={{x: 200, y: 300 }} data={graph} />
     </div>
   );
 }
