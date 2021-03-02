349a7d4acf4c6353bc692593fd77449bf8851cd3
Matthew Prestifilippo <matthew.prestifilippo@syapse.com>
2021-03-01 20:59:42 -0500

    Bind only the raw bucket too rabbit

--- conf/s3/init.sh
+++ conf/s3/init.sh
@@ -5,10 +5,14 @@ mc config host add s3 http://s3:9000 minio minio678
 mc admin config set s3/ notify_amqp:1 enable="on" exchange="s3" exchange_type="topic" no_wait="false"  url="amqp://admin:admin@rabbitmq:5672" auto_deleted="false" delivery_mode="0" durable="false" internal="false" routing_key=""
 mc admin service restart s3/
 
-for bucket in raw meta
+for bucket in raw meta data
 do
   echo "Add $bucket bucket"
   mc mb -p s3/$bucket
-  mc event add -p s3/$bucket arn:minio:sqs::1:amqp
 done
+
+
+mc event add -p s3/raw arn:minio:sqs::1:amqp
+
 echo "Done configuring minio instance"
+
